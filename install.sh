e1c82987d01d1bc57bbd0169ee095613635fa28a
derekking001 <derek.king@databricks.com>
2022-03-01 16:41:26 +0000

    updated

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
+# Simple script to execute the install of docker to the host, and then instantiate kafka and zookeeper containers.
 
 LOGFILE="/var/log/install.log"
 # Running the Docker and Docker-compose install script.
-
 if [ ! -e ./docker_install.sh ]
 then
-    echo "ERROR: docker_install.sh cannot be found - please check"
+    echo "ERROR: docker_install.sh cannot be found - please check" >>$LOGFILE
     exit 1
 else
     ./docker_install.sh
@@ -25,6 +25,9 @@ docker-compose -f docker/docker-compose.yml up -d
 ERROR=$?
 if [ $ERROR -ne 0 ]
     then
-    echo "error: $ERROR"
+    echo "cannot instantiate containers: (Error: $ERROR)"
     exit 1
 fi
+
+exit 0
+
